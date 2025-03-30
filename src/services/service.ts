427f28fc0e148f2da59377824f6107fbb4826399
Kongtup <ttansiri524@gmail.com>
2025-03-30 17:45:16 +0700

    feat: draft service single page

--- src/services/service.ts
+++ src/services/service.ts
@@ -25,3 +25,9 @@ export async function getServices(): Promise<Service[]> {
   const services: Service[] = await sanityClient.fetch(query);
   return services;
 }
+
+export async function getServiceBySlug(slug: string): Promise<Service | null> {
+  const query = `*[_type == "service" && slug.current == $slug][0]`;
+  const service = await sanityClient.fetch(query, { slug });
+  return service;
+}
